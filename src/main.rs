63465bdb21bb649b46abd623954fd2d350e44dbc
qdread <evanfincher18@gmail.com>
2024-02-28 17:50:42 -0500

    Added cusotm crosshair SHAPE (colors soon)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use glium::Surface;
 use winit::{platform::windows::WindowBuilderExtWindows, window::WindowBuilder};
 
@@ -6,7 +8,7 @@ use tray_icon::{TrayIconBuilder, menu::Menu, TrayIconEvent};
 #[macro_use]
 extern crate glium;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Vertex {
     position: [f32; 2],
 }
@@ -16,7 +18,7 @@ fn main() {
 
     // tray icon
     let tray_menu = Menu::new();
-    let tray_icon = TrayIconBuilder::new()
+    let _tray_icon = TrayIconBuilder::new()
     .with_menu(Box::new(tray_menu))
     .with_tooltip("Crosshair - Right click to close")
     .build()
@@ -25,31 +27,61 @@ fn main() {
 
     let event_loop = winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
     let window_builder = WindowBuilder::new()
-    .with_transparent(true)
-    .with_maximized(true)
-    .with_decorations(false)
-    .with_window_level(winit::window::WindowLevel::AlwaysOnTop)
-    .with_skip_taskbar(true)
-    .with_title("Crosshair");
+        .with_transparent(true)
+        .with_maximized(true)
+        .with_decorations(false)
+        .with_window_level(winit::window::WindowLevel::AlwaysOnTop)
+        .with_skip_taskbar(true)
+        .with_title("Crosshair");
 
 
     let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().set_window_builder(window_builder).build(&event_loop);
     let _ = _window.set_cursor_hittest(false);
     let mut frame = display.draw();
     frame.clear_color(0.0, 0.0, 0.0, 0.0);
+    // actual crosshair
+    let mut crosshair: Vec<Vertex> = vec![];
+    
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("crosshair.txt")
+        .unwrap();
+    
+    // Read the file contents into a string
+    let mut crosshair_file = String::new();
+    file.read_to_string(&mut crosshair_file).unwrap();
 
+    if crosshair_file == "" {
+        let num_points = 100; // The number of points to generate
+        let radius = 0.01; // The radius of the circle (1 is largest)
 
-    let num_points = 100; // The number of points to generate
-    let radius = 0.01; // The radius of the circle (1 is largest)
+        let (height, width) = display.get_framebuffer_dimensions();
+        let aspect_ratio = width as f32 / height as f32;
 
-    let (height, width) = display.get_framebuffer_dimensions();
-    let aspect_ratio = width as f32 / height as f32;
+        let circle: Vec<Vertex> = (0..num_points).map(|i: i32| {
+            let angle = i as f32 / num_points as f32 * 2.0 * std::f32::consts::PI;
+            Vertex { position: [radius * angle.cos() * aspect_ratio, radius * angle.sin()] }
+        }).collect();
+        crosshair = circle;
+    } else {
+        let mut vertex_entries = crosshair_file.split(";");
+        dbg!(&vertex_entries);
+        let mut vertex_vec: Vec<Vertex> = vec![];
+        for entry in vertex_entries {
+            let mut vertex = entry.split(",");
+            let x = vertex.next().unwrap().parse::<f32>().unwrap();
+            let y = vertex.next().unwrap().parse::<f32>().unwrap();
+            vertex_vec.push(Vertex { position: [x, y] });
+        }
+        dbg!(&vertex_vec);
+        crosshair = vertex_vec;
+    }
+    dbg!(&crosshair);
 
-    let circle: Vec<Vertex> = (0..num_points).map(|i| {
-        let angle = i as f32 / num_points as f32 * 2.0 * std::f32::consts::PI;
-        Vertex { position: [radius * angle.cos() * aspect_ratio, radius * angle.sin()] }
-    }).collect();
-    let vertex_buffer = glium::VertexBuffer::new(&display, &circle).unwrap();
+    
+    let vertex_buffer = glium::VertexBuffer::new(&display, &crosshair).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleFan);
 
     let vertex_shader_src = r#"
